52d1fc9b474cf6b3219593c712d1a0494a8ec935
Harun MISTIK <60209401+demoza@users.noreply.github.com>
2020-01-27 16:16:49 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -8,10 +8,8 @@ else
 echo 'Installing Requirements....'
 echo .
 echo .
-pkg install tsu
-apt install python3-pip
-pkg install python
 pip install requests
+pkg install python
 pip install colorama
 echo Made By Demoza >update.demoza
 echo Requirements Installed....
@@ -23,49 +21,51 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
-figlet Codanme:Reborn
+figlet Reborn
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border Demoza
-echo -e "\e[1;34m For Any Question Mail To Me!!\e[0m"
+echo -e "\e[4;34m Created By Demoza \e[0m"
+echo -e "\e[1;34m For Any Question Mail Me!!!\e[0m"
 echo -e "\e[1;32m           Mail: harunbusiness@aol.com \e[0m"
 echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/HarunMISTIK \e[0m"
 echo " "
+echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
 echo "Press 1 To  Start SMS Bomber "
-echo "Press 2 To  Update"
-echo "Press 99 To  Exit"
+echo "Press 2 To  Update "
+echo "Press 99 To  Exit "
 read ch
 if [ $ch -eq 1 ];then
 clear
 echo -e "\e[1;32m"
 rm *.xxx >/dev/null 2>&1
-python sms.py
+python3 bomber.py
 rm *.xxx >/dev/null 2>&1
 exit 0
 elif [ $ch -eq 2 ];then
 clear
 cd 
 rm -rf Reborn
+git clone https://github.com/demoza/Reborn
 clear
-git clone https://github.com/demoza/Reborn/
 cd Reborn
 chmod +x start.sh
-read a6
-./start.sh
-exit
+echo -e "\e[1;32m All The Required Packages Will Be Installed..."
+echo -e "\e[1;34m Please Restart..."
+exit 0
 elif [ $ch -eq 99 ];then
 clear
 echo -e "\e[1;31m"
 figlet Reborn
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border Demoza
-echo -e "\e[1;34m For Any Question Mail Me!!\e[0m"
+echo -e "\e[1;34m For Any Question Mail Me!!!\e[0m"
 echo -e "\e[1;32m           Mail: harunbusiness@aol.com \e[0m"
 echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/HarunMISTIK \e[0m"
 echo " "
 exit 0
 else
-echo -e "\e[4;32m Invalid Input !! \e[0m"
+echo -e "\e[4;32m Invalid Input !!! \e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
